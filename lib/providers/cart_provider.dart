9cbf780780933f3c67047406c795236cb5aa7124
Koko Ono <nyswzt.a@gmail.com>
2025-10-19 11:53:28 +0900

    init

---
+++ lib/providers/cart_provider.dart
@@ -0,0 +1,38 @@
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:riverpod_files/models/product.dart';
+
+part 'cart_provider.g.dart';
+
+@riverpod
+class CartNotifier extends _$CartNotifier {
+  //Initial value
+  @override
+  Set<Product> build() {
+    return {};
+  }
+
+  //Methods to update sate
+  void addProduct(Product product) {
+    if (!state.contains(product)) {
+      state = {...state, product};
+    }
+  }
+
+  void removeProduct(Product product) {
+    if (state.contains(product)) {
+      state = state.where((p) => p.id != product.id).toSet();
+    }
+  }
+}
+
+@riverpod
+int cartTotal(ref) {
+  final cartProducts = ref.watch(cartNotifierProvider);
+
+  int total = 0;
+
+  for (Product product in cartProducts) {
+    total += product.price;
+  }
+  return total;
+}
